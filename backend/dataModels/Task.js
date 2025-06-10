e7bebed1d910c0e3791880aefc107a283b72da34
KanikaSakthivel <kanikasakthivel15@gmail.com>
2025-06-10 09:46:15 +0530

    Update Task.js

--- backend/dataModels/Task.js
+++ backend/dataModels/Task.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 const taskSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  description: String,
-  dueDate: Date,
+  description: { type: String, default: '' },  // Default empty string for description
+  dueDate: { type: Date, default: Date.now },  // Default to current date if not provided
   status: { type: String, enum: ['Open', 'Complete'], default: 'Open' },
   userId: { type: String, required: true }
 });
