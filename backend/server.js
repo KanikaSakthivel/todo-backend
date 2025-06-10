d5cad0f18b864a53831d75485f56a07090265287
KanikaSakthivel <kanikasakthivel15@gmail.com>
2025-06-10 12:22:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: '*', // Your frontend URL
+  origin: 'https://todo-frontend-virid-rho.vercel.app', // Your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
 
